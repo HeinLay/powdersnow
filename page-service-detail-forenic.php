cf69d49237fab23daf1898ceac9051e299166835
ZINTHUHTET-P000\ZinThuHtet <scm.zinthuhtet@gmail.com>
2022-05-25 13:43:19 +0630

    - service_detail_forenic_page

---
+++ page-service-detail-forenic.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * The template for displaying Category
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package komiya
+ */
+
+get_header();
+/* Template Name: forenic */
+?>
+
+<section class="sec-mv service-forenic-mv">
+  <div class="l-inner">
+    <h2 class="mv-ttl"><span class="mvsub-ttl">SERVICE</span>フォレンジック調査・<br class="sp">対応</h2>
+  </div>
+</section><!-- ./sec-mv -->
+<div class="content">
+  <div id="service-detail-page" class="detail-forenic-page">
+    <div class="tabs-service-nav">
+      <ul class="tabs-service l-inner">
+        <li><a href="#service-overview">サービス概要</a></li>
+        <li><a href="#symptom-immediately">このような症状が思い当たる方は<br>すぐにご連絡をください</a></li>
+        <li><a href="#cyber-value">CYBER VALUEが<br>選ばれる理由</a></li>
+      </ul>
+    </div><!-- ./tabs-service-nav -->
+    <section class="service-detail-content l-inner">
+      <div class="service-detail-row" id="service-overview">
+        <h2 class="service-detail-ttl">サービス概要</h2>
+        <p class="service-txt">ホームページがサイバー攻撃により改ざんされた、サーバーがダウンした、といったセキュリティ事故時にセキュリティエンジニアが攻撃箇所の修復、セキュリティ対策措置など緊
+        急対応を実施するサービスです。<br>
+        どのように侵入されたのか、情報流出したのか、していないのか、まだ攻撃は続いているのか、早急に調査・対策します。</p>
+      </div>
+      <div class="service-detail-row" id="symptom-immediately">
+        <h2 class="service-detail-ttl">このような症状が思い当たる方はすぐにご連絡をください</h2>
+        <div class="symptom-list">
+          <p>・サイトや管理画面にアクセスできない</p>
+          <p>・覚えのないコンテンツや動作がある</p>
+          <p>・メールが送信できない</p>
+          <p>・迷惑メールが届く</p>
+          <p>・Wordpressで作ったサイトが改ざんされた、ダッシュボードがおかしくなった</p>
+          <p>・会社のブログでスパムコメントが来るようになった</p>
+        </div>
+        <p>⇒ こういった症状がある場合、情報改ざんなど明らかな被害でなく、気づきにくい形でセキュリティ事故を起こしていたり、サイバー被害にあっていたりする可能性が高いです。</p>
+      </div>
+      <div class="service-detail-row" id="cyber-value">
+        <h2 class="service-detail-ttl">CYBER VALUEが選ばれる理由</h2>
+        <p class="detail-list-txt">① 最短当日のスピード対応</p>
+        <p>サイバー攻撃は時間勝負です。最短で当日打ち合わせ、翌日から復旧作業へ入れます。対応は社内チームで完結するためスムーズ、スピーディです。</p>
+        <p class="detail-list-txt">② 初期調査は無料</p>
+        <p>初期調査は無料です。初期調査が完了次第、正式な御見積をご提示します。この段階でお断りとなっても費用は掛かりません。</p>
+        <p class="detail-list-txt">③ 多数の実績</p>
+        <p>CYBER VALUE では、キャリア10年以上の経験豊富なエンジニアが調査、対応いたします。被害箇所や影響範囲の指摘が速く、的確です。</p>
+        <p class="detail-list-txt">④ 人の手による柔軟な対応</p>
+        <p>エンジニアによる人の手で調査を行います。そのため、どんな環境や状況の事故にも、柔軟に対応可能です。</p>
+        <p class="detail-list-txt">⑤ 会見や弁護士対応も可能</p>
+        <p>業種や事故状況から、適切な報告書作成をサポート。記者会見や、弁護士を必要とする案件も実績があります。</p>
+      </div>
+    </section><!-- ./service-detail-content -->
+  </div><!-- #/service-detail-page -->
+</div><!-- ./content -->
+<?php 
+get_footer() ?>
