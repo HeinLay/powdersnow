424e07bc1fef291f9252ee8e26949e075b2d83f5
ZINTHUHTET-P000\ZinThuHtet <scm.zinthuhtet@gmail.com>
2022-05-25 11:43:14 +0630

    - service_detail_security

---
+++ page-service-detail-rumors.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * The template for displaying Category
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package komiya
+ */
+
+get_header();
+/* Template Name: rumors */
+?>
+
+<section class="sec-mv service-rumors-mv">
+  <div class="l-inner">
+    <h2 class="mv-ttl"><span class="mvsub-ttl">SERVICE</span>風評被害対策</h2>
+  </div>
+</section><!-- ./sec-mv -->
+<div class="content">
+  <div id="service-detail-page" class="detail-rumors-page">
+    <div class="tabs-service-nav">
+      <ul class="tabs-service l-inner">
+        <li><a href="#service-overview">サービス概要</a></li>
+        <li><a href="#cyber-value">CYBAR VALUEが選ばれる理由</a></li>
+      </ul>
+    </div><!-- ./tabs-service-nav -->
+    <section class="service-detail-content l-inner">
+      <div class="service-detail-row" id="service-overview">
+        <h2 class="service-detail-ttl">サービス概要</h2>
+        <p class="service-txt">会社名やサービス名、個人名などの「指定キーワード」をYahoo!やGoogleで検索したときに表示されるネガティブなサイトを見えにくくするサービスです。
+          検索結果にネガティブなキーワードが表示されてしまうと、それだけでユーザーは悪い印象を受け、ビジネスに大きな悪影響を与えかねません。
+          （ネガティブなキーワードの例：「社名 不祥事」「社名 ブラック」「社名 裁判」「社名 反社」など）</p>
+        <p class="effect-txt">風評被害の悪影響</p>
+        <div class="effects-blk">
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_sales_decline.png" alt="売上低下"></div>
+            <p>売上低下</p>
+          </div>
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_stock_price.png" alt="株価低下"></div>
+            <p>株価低下</p>
+          </div>
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_interruption.png" alt="取引中断"></div>
+            <p>取引中断</p>
+          </div>
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_bank.png" alt="融資の拒否"></div>
+            <p>融資の拒否</p>
+          </div>
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_site.png" alt="サイトへのアクセス激減"></div>
+            <p>サイトへのアクセス激減</p>
+          </div>
+          <div class="effect-item">
+            <div class="effect-icon"><img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/ico_form.png" alt="採用の応募激減で人材確保困難"></div>
+            <p>採用の応募激減で人材確保困難</p>
+          </div>
+        </div>
+      </div>
+      <div class="service-detail-row" id="cyber-value">
+        <h2 class="service-detail-ttl">CYBER VALUE が選ばれる理由</h2>
+        <p class="detail-list-txt">① SEO会社による最適な施策</p>
+        <p>CYBER VALUEでは過去10年間、SEO対策をメイン事業とし、つねに最新の情報を収集しています。そのため、検索エンジンのアルゴリズムや関連キーワード・サジェストの仕組み
+          などを熟知、ノウハウを蓄積しており、それを利用した最適な施策が実施可能です。</p>
+        <p class="detail-list-txt">② 短期間で効果がわかる</p>
+        <p>CYBER VALUE では、検索エンジンから高評価を受け、あらゆるキーワードで上位表示を達成している高品質なサイトを所有しています。これを活用することにより、短期間で「ネ
+          ガティブなキーワードを含むサイト」を押し下げることが可能です。</p>
+        <p class="detail-list-txt">③ 施策はすべて見える化 </p>
+        <p>他社にも同様のサービスはありますが、どのような方法を用いているのか、本当に施策を実施しているのか、不透明なものが多いです。その点、CYBER VALUE では施策を見える化
+          し、状況も逐一お伝えいたしますので、安心してご利用いただけます。
+          <br><br>
+          自社で保有しているサイトや、当社で新規作成したサイトを上位表示させることで、ネガティブワードを含む風評サイト（悪質なサイト）を検索順位から押し下げる施策を実施しま
+          す。創業以来、検索サイトのアルゴリズムやSEO対策を研究し続け、ノウハウを蓄積してきた CYBER VALUE だからこそ提供できるサービスです。
+        </p>
+        <div class="search-result-img">
+          <img src="<?php bloginfo('template_directory'); ?>/assets/images/service-detail/img_search_results.png" alt="">
+        </div>
+        <p>風評被害対策は通常のSEO対策とは考え方も施策も異なり、「逆SEO対策」と呼ばれます。この逆SEO対策は、通常のSEO対策より作業量が多く、難易度が高くなります。それで
+          も、なぜ当社は施策を成功させられるのか、その理由をご紹介いたします。</p>
+        <fieldset class="roadmap-survey">
+          <legend>当社の「逆SEO対策」の成功率が高い理由</legend>
+          <dl class="reason-chosen-list">
+            <dt class="detail-list-txt">① なぜネガティブサイトが上位か徹底分析を実施 </dt>
+            <dd>順位を下げたいサイトがなぜ上位にあるのか、どうすればほかのサイトを上位に上げられるのか、が分からなければ施策のし
+              ようがありません。ロードマップでは施策の第一段階として、まずネガティブサイトを徹底分析し、どう施策を進めていくの
+              か、その戦略を分かりやすくご説明いたします。</dd>
+          </dl>
+          <dl class="reason-chosen-list">
+            <dt class="detail-list-txt">② 検索エンジンで高評価を受ける自社サイトを活用</dt>
+            <dd>1からサイトを作成してネガティブサイトを下げようとしても、検索サイトで高評価を得て上位表示を達成するには、長い時間
+              がかかり、また確実に成功するわけではありません。しかし、CYBER VALUE は前述のように高品質なサイトを所有している
+              ため、これを活用し、早く、効果的に結果を出すことが可能です。</dd>
+          </dl>
+          <dl class="reason-chosen-list">
+            <dt class="detail-list-txt">③ 自社オリジナルコンテンツを常時配信できる体制</dt>
+            <dd>検索サイトで高評価を得て上位表示できるサイトの条件とは、つねに質の高いオリジナルコンテンツを発信し続けていること
+              です。CYBER VALUE には SEO の知識のある専属ライターが複数名常駐しており、外注する必要がなく、いつでも高品質なオ
+              リジナルコンテンツを配信し、スピーディーに施策を進められます。
+              風評被害対策サービスを提供する企業はいくつかありますが、そのすべてが同様のノウハウ、技術を有するわけではありませ
+              ん。業者選びをされる際にどのような部分をチェックすべきか、注意点をご紹介いたします。</dd>
+          </dl>
+          <div class="service-security-blk">
+            <div class="service-security">
+              <h3>サイバー攻撃による<br>施策を実施する業者は要注意 </h3>
+              <p>ネガティブサイトの順位を下げるために、サイバー
+                攻撃やスパムなどを実施する業者もいます。しか
+                し、そもそもサイバー攻撃は犯罪でありリスクが高
+                く、このような業者に依頼した企業だと広まれば、
+                さらなる悪評を招きかねません。</p>
+            </div>
+            <div class="service-security">
+              <h3>被リンク対策だけを謳う業者は要注意 </h3>
+              <p>逆SEO対策のノウハウがない業者は、被リンク施策
+                をおこなうと言うだけの場合が多いです。実際、被
+                リンク施策だけでは有効な手法といえません。ま
+                た、さらに悪質な業者だと施策の状況や進捗も伝え
+                ず、実際はなにもしないで順位が下がるのを待つだ
+                けというケースもあります。</p>
+            </div>
+          </div>
+        </fieldset>
+      </div>
+    </section><!-- ./service-detail-content -->
+  </div><!-- #/service-detail-page -->
+</div><!-- ./content -->
+<?php 
+get_footer() ?>
